2f67d495584829d9e8da33f733327329bb90bbf5
Felipe Trevisan <felipejs@gmail.com>
2025-03-10 20:52:49 -0300

    chore: add types

---
+++ src/components/contact-form-dialog.tsx
@@ -0,0 +1,21 @@
+import { useApp } from "@/hooks/use-app";
+import { ContactForm } from "./contact-form";
+import { DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
+
+export function ContactFormDialog() {
+  const { setIsContactDialogOpen } = useApp();
+
+  const closeDialog = () => {
+    setIsContactDialogOpen(false);
+  }
+  
+  return (
+    <DialogContent className="container max-w-3xl" onEscapeKeyDown={closeDialog}>
+      <DialogHeader>
+        <DialogTitle>Como podemos te ajudar?</DialogTitle>
+      </DialogHeader>
+
+      <ContactForm isDialog onClose={closeDialog} />
+    </DialogContent>
+  );
+}
---
+++ src/types/testimonial.ts
@@ -0,0 +1,7 @@
+export type Testimonial = {
+  id: string;
+  author: {
+    name: string;
+  };
+  testimonial?: string;
+};
