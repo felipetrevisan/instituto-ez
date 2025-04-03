de5d9e664039b1e4db5d2cba959a8204a4260b90
Felipe Trevisan <felipejs@gmail.com>
2025-04-03 20:28:26 -0300

    chore: add prettier plugin

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,12 @@
 /** @type {import("prettier").Config} */
 const config = {
-  trailingComma: 'es5',
+  trailingComma: "es5",
   semi: true,
   singleQuote: false,
   bracketSpacing: true,
-  arrowParens: 'always',
+  arrowParens: "always",
   printWidth: 100,
+  plugins: ["prettier-plugin-tailwindcss"],
 };
 
 module.exports = config;
