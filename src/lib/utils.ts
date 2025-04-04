1e00767fdf6d4bd7b9304fe4df92f48f474da843
Felipe Trevisan <felipejs@gmail.com>
2025-04-03 22:00:57 -0300

    feat: create helper link

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx";
+import { Button, LinkType } from "@/types/global";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function getLink(button: Button) {
+  return button.type === LinkType.INTERNAL ? `/${button.link}` : button.externalUrl!;
 }
