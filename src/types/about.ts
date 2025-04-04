c3b67303cea7dd20d54ad7f60ebdcf84b5a7e343
Felipe Trevisan <felipejs@gmail.com>
2025-04-04 02:21:44 -0300

    chore: remove unused files

--- src/types/about.ts
+++
@@ -1,9 +0,0 @@
-import { PortableTextBlock } from "sanity";
-import { SanityAsset } from "./sanityAssets";
-
-export type About = {
-  id: string;
-  title?: string;
-  content: PortableTextBlock[];
-  background: SanityAsset;
-};
