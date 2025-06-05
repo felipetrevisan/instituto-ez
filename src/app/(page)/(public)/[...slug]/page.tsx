4ee48c4514ae709e57a1d360fe5ab9673855ad5b
Felipe Trevisan <felipejs@gmail.com>
2025-06-05 16:37:45 -0300

    feat: add animation scroll

--- src/app/(page)/(public)/[...slug]/page.tsx
+++ src/app/(page)/(public)/[...slug]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { ScrollToHash } from '@/components/scroll-to-hash';
 import { usePage } from '@/hooks/use-page';
 import { use } from 'react';
 import { Content } from './_content';
@@ -9,8 +10,6 @@ export default function Page({
 }: { params: Promise<{ slug: string }> }) {
 	const { slug: [slug] } = use(params);
 
-	console.log(slug);
-
 	const { data, isLoading } = usePage(slug);
 
 	if (isLoading || !data) return <></>;
@@ -18,6 +17,7 @@ export default function Page({
 	return (
 		<div className="w-full flex items-center flex-col justify-center space-y-14">
 			<section className="section relative flex flex-col gap-20 w-screen items-center justify-center">
+				<ScrollToHash />
 				{data.sections.map((section, index) => (
 					<Content {...section} key={`${data.id}-${data.slug}-${index}`} />
 				))}
