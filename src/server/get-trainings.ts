54255873eca56a6ad3e3d2146342528524530f95
Felipe Trevisan <felipejs@gmail.com>
2025-06-12 17:27:01 -0300

    fix: remove training section

--- src/server/get-trainings.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-
-import { sanityFetch } from '@/sanity/lib/fetch';
-import { trainingQuery } from '@/sanity/lib/queries';
-import type { Training } from '@/types/training';
-
-export async function getTrainings() {
-	return sanityFetch<Training[]>({ query: trainingQuery });
-}
