8d3368fa5e07b49f04a089784649f346285d0dea
Felipe Trevisan <felipejs@gmail.com>
2025-09-05 14:36:16 -0300

    feat: add custom heading component

--- apps/studio/src/portable/index.ts
+++ apps/studio/src/portable/index.ts
@@ -1,4 +1,5 @@
 import type { PortableTextComponents } from '@portabletext/react'
+import { portableBlocks } from './block'
 import { portableLists } from './lists'
 import { portableMarks } from './marks'
 
@@ -6,5 +7,6 @@ export function createPortableComponents(): PortableTextComponents {
   return {
     marks: portableMarks,
     list: portableLists,
+    block: portableBlocks,
   }
 }
