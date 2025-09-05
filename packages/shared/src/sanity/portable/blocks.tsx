67178588af8a1a909879856d4d5f2e7639806339
Felipe Trevisan <felipejs@gmail.com>
2025-09-05 14:48:37 -0300

    fix: heading component portable text

--- packages/shared/src/sanity/portable/blocks.tsx
+++ packages/shared/src/sanity/portable/blocks.tsx
@@ -1,5 +1,11 @@
+/** biome-ignore-all lint/suspicious/noExplicitAny: <explanation> */
 import type { PortableTextComponents } from '@portabletext/react'
 
 export const portableBlocks: PortableTextComponents['block'] = {
+  h1: ({ children }: any) => <h1 className="my-6 font-bold text-4xl">{children}</h1>,
+  h2: ({ children }: any) => <h2 className="my-5 font-semibold text-3xl">{children}</h2>,
+  h3: ({ children }: any) => <h3 className="my-4 font-medium text-2xl">{children}</h3>,
+  h4: ({ children }: any) => <h4 className="my-3 font-medium text-xl">{children}</h4>,
+  h5: ({ children }: any) => <h5 className="my-2 font-medium text-lg">{children}</h5>,
   normal: ({ children }) => <div>{children}</div>,
 }
