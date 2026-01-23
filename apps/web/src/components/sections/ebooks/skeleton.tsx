42d4590352732273d87480643b543e7bbe7f9a38
Felipe Trevisan <felipejs@gmail.com>
2026-01-23 01:20:47 -0300

    fix: fix error

---
+++ apps/web/src/components/sections/ebooks/skeleton.tsx
@@ -0,0 +1,11 @@
+import { Skeleton as BaseSkeleton } from '@ez/shared/ui/skeleton'
+
+export function Skeleton() {
+  return (
+    <>
+      <BaseSkeleton className="h-96 w-80" />
+      <BaseSkeleton className="h-96 w-80" />
+      <BaseSkeleton className="h-96 w-80" />
+    </>
+  )
+}
