bf359af6d3681c1100f61119ae2e7e395bab5e3e
Felipe Trevisan <felipejs@gmail.com>
2026-02-05 14:50:05 -0300

    fix: remove margin

--- apps/web/src/components/navigation/landing/footer.tsx
+++ apps/web/src/components/navigation/landing/footer.tsx
@@ -5,7 +5,7 @@ import { type HTMLMotionProps, motion } from 'motion/react'
 
 export const FooterLandingNavigation = ({ className, children }: HTMLMotionProps<'footer'>) => {
   return (
-    <motion.footer className={cn('mt-20 bg-footer py-16 text-footer-foreground', className)}>
+    <motion.footer className={cn('bg-footer py-16 text-footer-foreground', className)}>
       {children}
     </motion.footer>
   )
